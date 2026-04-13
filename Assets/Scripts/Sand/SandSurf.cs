b4bb57e77bea8bd3c5cbc5e95aa16e7c7cbc3c14
Emma Lundquist <em649650@ucf.edu>
2026-04-13 00:00:41 -0400

    i am cooked

--- Assets/Scripts/Sand/SandSurf.cs
+++
@@ -1,9 +0,0 @@
-void surf (Input IN, inout SurfaceOutput o)
-{
-    o.Albedo = _SandColor;
-    o.Alpha = 1;
-    float3 N = float3(0, 0, 1);
-    N = RipplesNormal(N);
-    N = SandNormal   (N);
-    o.Normal = N;
-}
