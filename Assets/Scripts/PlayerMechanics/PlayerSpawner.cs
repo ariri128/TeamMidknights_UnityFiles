8a8b211c0c831909a3d792891f47812ab84b4528
ariri128 <arya.v.kamat@gmail.com>
2026-04-22 04:32:26 -0400

    cleanup script folder

--- Assets/Scripts/PlayerMechanics/PlayerSpawner.cs
+++ Assets/Scripts/PlayerMechanics/PlayerSpawner.cs
@@ -27,31 +27,4 @@ public class PlayerSpawner : MonoBehaviour
             }
         }
     }
-
-    /*
-    public Transform spawnPoint;
-
-    private CharacterController controller;
-
-    private void Awake()
-    {
-        controller = GetComponent<CharacterController>();
-
-        if (spawnPoint != null)
-        {
-            if (controller != null)
-            {
-                controller.enabled = false;
-            }
-
-            transform.position = spawnPoint.position;
-            transform.rotation = spawnPoint.rotation;
-
-            if (controller != null)
-            {
-                controller.enabled = true;
-            }
-        }
-    }
-    */
 }
